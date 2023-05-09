9d6b3afdef17487b4d8c11e1ebbe5b6ec8b49a27
Paolo Licup <paolo.licup@technomancer.biz>
2023-05-09 22:55:21 +0800

    database module and various fixes with auth module

--- server.js
+++ server.js
@@ -7,32 +7,44 @@
 */
 
 async function main() {
+    const express = require('express')
+    const app = express()
 
-  const express = require('express')
-  const app = express()
-  
-  require('dotenv').config()
-  
-  const bodyParser = require('body-parser')
-  app.use(bodyParser.json())
+    require('dotenv').config()
 
-  // Serving frontend
-  const path = require('path')
-  app.use(express.static(path.join(__dirname, 'dist')))
-  // end Serving frontend
+    const bodyParser = require('body-parser')
+    app.use(bodyParser.json())
 
-  // Fallback
-  app.get('*', (req, res) => {
-    res.sendFile(__dirname + '/dist/index.html');
-  })
+    // Database Module
+    const database = require('./database/database')
+    const initial = require('./database/initial')
+    // end Database Module
 
-  var history = require('connect-history-api-fallback')
-  app.use(history())
-  // end Fallback
+    // Auth Module
+    const auth = require('./auth/auth')
+    const authDb = await database.openOrCreateDB('./auth/auth.sqlite')
+    await initial.createInitialTables(authDb)
+    await initial.createInitialRows(authDb)
+    await auth.main(app, authDb, './auth/auth.sqlite')
+    app.use(auth.router)
+    // end Auth Module
 
-  app.listen(process.env.PORT, () => {
-    console.log('Express app listening on port', process.env.PORT)
-  })
+    // Serving frontend
+    const path = require('path');
+    app.use(express.static(path.join(__dirname, 'dist')))
+    // end Serving frontend
 
+    // Fallback
+    app.get('*', (req, res) => {
+        res.sendFile(__dirname + '/dist/index.html')
+    });
+
+    var history = require('connect-history-api-fallback')
+    app.use(history())
+    // end Fallback
+
+    app.listen(process.env.PORT, () => {
+        console.log('Express app listening on port', process.env.PORT)
+    });
 }
-main()
+main();
