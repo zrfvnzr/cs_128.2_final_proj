487e3ca2980e767a97be47e12040808959d502d4
Paolo Licup <paolo.licup@technomancer.biz>
2023-05-09 23:18:30 +0800

    added basic frontend folder

---
+++ frontend/src/App.vue
@@ -0,0 +1,21 @@
+<script>
+</script>
+
+<template>
+  <div>
+    <a href="https://vitejs.dev" target="_blank">
+      <img src="/vite.svg" class="logo" alt="Vite logo" />
+    </a>
+    <a href="https://vuejs.org/" target="_blank">
+      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
+    </a>
+  </div>
+</template>
+
+<style>
+* {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+}
+</style>
