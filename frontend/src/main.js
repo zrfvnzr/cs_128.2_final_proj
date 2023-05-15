443b2cfd03223f2d75999f2c6777c0616a1d3991
Paolo Licup <paolo.licup@technomancer.biz>
2023-05-15 23:21:53 +0800

    installed vue-router and vue-axios

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -2,4 +2,27 @@ import { createApp } from 'vue'
 import App from './App.vue'
 
 const app = createApp(App)
+
+// Vue Router
+import { createRouter, createWebHistory } from 'vue-router'
+
+// import route components
+
+// set routes
+const routes = [
+//   {path: '/', component: Home}
+]
+
+const router = createRouter({
+  history: createWebHistory(), routes
+})
+app.use(router)
+// end Vue Router
+
+// Vue Axios
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+app.use(VueAxios, axios)
+// end Vue Axios
+
 app.mount('#app')
