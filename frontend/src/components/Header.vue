6d27cb8ccd2fec9d7a85ab885c4231c6599d13ab
Paolo Licup <paolo.licup@technomancer.biz>
2023-05-17 13:04:28 +0800

    initial commit

---
+++ frontend/src/components/Header.vue
@@ -0,0 +1,60 @@
+<script>
+export default {
+        name: Header
+}
+</script>
+
+<template>
+<div class="container">
+    <div id="upimgHeader">
+        <a href=""><h5 style="display: inline;">Predictor</h5></a> 
+        <a href=""><h5 style="display: inline; padding-left: 50px ;">History</h5></a> 
+        <a href="">
+                <div style="display: inline; padding-left: 420px ;">
+                        <h1 style="display: inline; font-weight:500; color: #5BB95A;">DEEP</h1>
+                        <h1 style="display: inline; font-weight:700; color: #00A0DC;">DR</h1>
+                </div>
+        </a> 
+        <button id="signoutButton" style="color: white">Sign Out</button>
+    </div>
+</div>
+</template>
+
+<style>
+.container{
+    max-width: 100%;
+    height: 90px;
+    background-image: url('background1 (2).jpg');
+}
+a {
+    color: black;
+    text-decoration: none;
+}
+a:hover {
+    color: #5BB95A;
+    background-color: transparent;
+    text-decoration: none;
+}
+#upimgHeader{
+    padding-top: 10px;
+    padding-left: 50px;
+    color: black;
+}
+#signoutButton {
+    margin-left: 470px; 
+    position: relative; 
+    font-size: 15px; 
+    width: 10%;
+    background-color: #8BC34A;
+    border-radius: 5px;
+    border-color:  #C7C7CC;
+}
+#signoutButton:hover{
+    background-color: #5BB95A;
+}
+#signoutButton:active {
+    background-color: #5BB95A;
+    box-shadow: 0 5px #666;
+    transform: translateY(1px);
+}
+</style>
---
+++ frontend/src/views/Login.vue
@@ -0,0 +1,90 @@
+<script>
+export default {
+    name: 'Login'
+}
+</script>
+
+<template>
+<div class="d-flex" id="container">
+    <div id="loginDiv" >
+      <div style="margin-top: 50px; margin-left: 225px;">
+        <h1 style="display: inline; font-weight:700; color: #5BB95A;">DEEP</h1>
+        <h1 style="display: inline; font-weight:800; color: #00A0DC;">DR</h1>
+      </div>
+      <form>
+      <div class="form-group">
+        <label for="username">Username <h6 style="display: inline; color: red;">*</h6></label>
+        <input type="text" class="form-control" name="username" id="username" placeholder="Required">
+      </div>
+      <div class="form-group">
+        <label for="username">Password <h6 style="display: inline; color: red;">*</h6></label>
+        <input type="text" class="form-control" name="password" id="password" placeholder="Required">
+      </div>
+      <div class="form-group">
+        <input type="checkbox" class="form-check-input" id="remember">
+        <label class="form-check-label" for="remember" style="padding-left: 5px; opacity: 0.75;">Remember me</label>
+      </div>
+      </form>
+      <button id="loginButton" style="color: white; font-weight: 500;">Login</button>
+    </div>
+  </div>
+</template>
+<button id="uploadButton" style="color:white; font-weight: 500;">Select Image</button>
+
+<style>
+#container{
+  max-width: 100%;
+  height: 100vh;
+  background-image: url('background1 (2).jpg');
+}
+#loginDiv {
+  margin: auto;
+  margin-top: 120px;
+  width: 40%;
+  height: 500px;
+  background-color: #F2F2F2;
+  border-width: 1px;
+  border-style: solid;
+  border-radius: 10px;
+  border-color:#0072C6 ;
+}
+#loginButton {
+  margin-left: 240px; 
+  font-size: 15px; 
+  width: 20%;
+  background-color: #8BC34A;
+  border-radius: 5px;
+  border-color:  #C7C7CC;
+  justify-content: center;
+  align-items: center;
+  display: flex;
+  margin-top: 5%;
+}
+#loginButton:hover{
+  background-color: #5BB95A;
+}
+#loginButton:active {
+  background-color: #5BB95A;
+  box-shadow: 0 5px #666;
+  transform: translateY(1px);
+}
+.form-group{
+  margin-top: 30px;
+  margin-left: 100px;
+
+}
+input[type=text]{
+  border-width: 1px;
+  border-style: solid;
+  border-radius: 6px;
+  border-color:#5BB95A ;
+  width: 400px;
+}
+input[type=text]::-webkit-input-placeholder{
+  opacity: 0.5;
+}
+input[type=checkbox]{
+  background-color: #D9D9D9;
+}
+
+</style>
