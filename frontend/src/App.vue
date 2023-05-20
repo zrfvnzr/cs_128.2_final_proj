a2764395c64badb8e660df23cedaee5bd924af7b
Paolo Licup <paolo.licup@technomancer.biz>
2023-05-20 14:26:54 +0800

    installed bootstrap icons
    some progress to admin dashboard view users div

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -13,4 +13,36 @@
   margin: 0;
   padding: 0;
 }
+:root {
+  --apple: #5BB95A;
+  --rich-electric-blue: #00A0DC;
+}
+.hoverTransform {
+  transition: transform .1s linear;
+}
+.hoverTransform:hover {
+  transform: scale(.95);
+}
+.h-1-rem {
+  height: 1rem;
+}
+/* font colors */
+.fc-apple {
+  color: var(--apple);
+}
+/* end font colors */
+/* font sizes */
+.fs-7 {
+  font-size: .9rem;
+}
+.fs-8 {
+  font-size: .8rem;
+}
+.fs-9 {
+  font-size: .7rem;
+}
+.fs-10 {
+  font-size: .6rem;
+}
+/* end font sizes */
 </style>
--- frontend/src/views/Admin.vue
+++ frontend/src/views/Admin.vue
@@ -1,49 +1,98 @@
 <script>
 export default {
-  name: 'Admin'
+  name: 'Admin',
+  data() {
+    return {
+        users: [
+            {
+                id: 1,
+                role: 'admin',
+                username: 'admin1',
+                firstName: 'Paolo',
+                lastName: 'Licup'
+            },
+            {
+                id: 2,
+                role: 'admin',
+                username: 'admin2',
+                firstName: 'Emmanuel',
+                lastName: 'Ednalan'
+            }
+        ]
+    }
+  }
 }
 </script>
 <template>
-<div class="p-4">
-    <h1>Admin View</h1>
+<div style="padding: 3rem 3rem 0 3rem;">
 
     <!-- Users Index -->
-    <div ref="usersIndexDiv" class="border border-2 border-dark mb-3">
-        <div class="p-3">
-            <h2>Users Index</h2>
-            <table class="table table-hover table-sm">
-                <thead>
+    <div ref="usersIndexDiv">
+        <!-- Users Index Header -->
+        <div class="d-flex justify-content-between mb-2">
+            <div class="d-flex fc-apple">
+                <i class="bi bi-people-fill fs-3 h-1-rem lh-1 me-1"></i>
+                <h1 class="fs-3 fw-bold lh-1 m-0">Admin Dashboard</h1>
+            </div>
+            <div class="align-items-center d-flex">
+                <input class="h-100 lh-1 me-1 px-1" type="text" placeholder="Search users...">
+                <div class="btn btn-sm btn-success hoverTransform lh-1 p-2">Search</div>
+            </div>
+        </div>
+        <!-- end Users Index Header -->
+        <!-- Users Index Table -->
+        <div class="mb-3">
+            <table class="table table-hover table-sm text-center">
+                <thead class="text-white" style="background-color: var(--rich-electric-blue);">
                     <tr>
-                        <th scope="col">#</th>
-                        <th scope="col">Username</th>
+                        <th scope="col">ID</th>
                         <th scope="col">Role</th>
+                        <th scope="col">Username</th>
+                        <th scope="col">First Name</th>
+                        <th scope="col">Last Name</th>
+                        <th scope="col">Edit</th>
+                        <th scope="col">Delete</th>
                     </tr>
                 </thead>
                 <tbody>
-                    <tr>
-                        <th scope="row">1</th>
-                        <td>admin1</td>
-                        <td>admin</td>
-                    </tr>
-                    <tr>
-                        <th scope="row">2</th>
-                        <td>admin2</td>
-                        <td>admin</td>
-                    </tr>
-                    <tr>
-                        <th scope="row">3</th>
-                        <td>doctor1</td>
-                        <td>doctor</td>
+                    <tr v-for="(obj, index) in users" :key="index">
+                        <th scope="row">{{ users[index].id }}</th>
+                        <td class="text-capitalize">{{ users[index].role }}</td>
+                        <td>{{ users[index].username }}</td>
+                        <td class="text-capitalize">{{ users[index].firstName }}</td>
+                        <td class="text-capitalize">{{ users[index].lastName }}</td>
+                        <td>
+                            <div class="btn btn-sm btn-warning hoverTransform">Edit</div>
+                        </td>
+                        <td>
+                            <div class="btn btn-danger btn-sm hoverTransform">Delete</div>
+                        </td>
                     </tr>
                 </tbody>
             </table>
         </div>
+        <!-- end Users Index Table -->
     </div>
     <!-- end Users Index -->
 
     <!-- User Edit -->
+    <div ref="userEditDiv">
+        <!-- User Edit Header -->
+        <div class="d-flex justify-content-between mb-2">
+            <div class="d-flex fc-apple">
+                <i class="bi bi-person-fill-gear fs-3 h-1-rem lh-1 me-1"></i>
+                <h1 class="fs-3 fw-bold lh-1 m-0">Edit User</h1>
+            </div>
+        </div>
+        <!-- end User Edit Header -->
+        <!-- User Edit Body -->
+        <!-- end User Edit Body -->
+    </div>
+    <!-- end User Edit -->
+
+    <!-- Old User Edit -->
     <div ref="userEditDiv" class="border border-2 border-dark mb-3 p-3">
-        <h2>Edit User</h2>
+        <h2>Old Edit User</h2>
         <div class="align-items-start d-flex flex-column">
             <label>Username</label>
             <input type="text">
@@ -56,11 +105,11 @@ export default {
             </select>
         </div>
     </div>
-    <!-- end User Edit -->
+    <!-- end  OldUser Edit -->
 
-    <!-- SQL Console -->
+    <!-- Old SQL Console -->
     <div ref="sqlConsoleDiv" class="border border-2 border-dark p-3">
-        <h2>SQL Console</h2>
+        <h2>Old SQL Console</h2>
         <div class="align-items-start d-flex flex-column">
             <label>Execute SQL</label>
             <textarea cols="100" rows="5"></textarea>
@@ -69,7 +118,7 @@ export default {
         </div>
         
     </div>
-    <!-- end SQL Console -->
+    <!-- end Old SQL Console -->
 
 </div>
 </template>
